9697aecea23cd55f9da7502c99abe8bcd9bd9176
Swapnil <swapnil1988@gmail.com>
2015-09-21 02:12:17 +0530

    getting user from db

--- user-notes-dao/src/main/java/com/gotprint/impl/UserHibernateDaoImpl.java
+++ user-notes-dao/src/main/java/com/gotprint/impl/UserHibernateDaoImpl.java
@@ -1,12 +1,22 @@
 package com.gotprint.impl;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
 import com.gotprint.dao.UserHibernateDao;
 import com.gotprint.domain.User;
+import com.gotprint.util.HibernateUtil;
 
 public class UserHibernateDaoImpl implements UserHibernateDao {
 
 	public User getUserDetails(String userEmail) {
-		return null;
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction tx = session.beginTransaction();
+        User user = (User) session.get(User.class, userEmail);
+        tx.commit();
+        sessionFactory.close();
+		return user;
 	}
-
 }
