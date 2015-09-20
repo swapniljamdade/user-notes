659e8b16da0c163550e4071c4a4d47678083eb15
Swapnil <swapnil1988@gmail.com>
2015-09-21 02:00:02 +0530

    Hibernate entities and cfg.xml

--- user-notes-dao/src/main/java/com/gotprint/domain/Note.java
+++ user-notes-dao/src/main/java/com/gotprint/domain/Note.java
@@ -1,5 +1,70 @@
 package com.gotprint.domain;
 
-public class Note {
+import java.sql.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "note")
+public class Note {
+	
+	@ManyToOne
+	@JoinColumn(name="userEmail")
+	private User user;
+	
+	@Column(name = "title")
+	private String title;
+	
+	@Column(name = "note")
+	private String note;
+	
+	@Column(name = "timeCreated")
+	private Date timeCreated;
+	
+	@Column(name = "lastUpdated")
+	private Date lastUpdated;
+
+	
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getNote() {
+		return note;
+	}
+	public void setNote(String note) {
+		this.note = note;
+	}
+	public Date getTimeCreated() {
+		return timeCreated;
+	}
+	public void setTimeCreated(Date timeCreated) {
+		this.timeCreated = timeCreated;
+	}
+	public Date getLastUpdated() {
+		return lastUpdated;
+	}
+	public void setLastUpdated(Date lastUpdated) {
+		this.lastUpdated = lastUpdated;
+	}
+	
+	@Override
+	public String toString() {
+		return "Note [userEmail=" + user + ", title=" + title + ", note="
+				+ note + ", timeCreated=" + timeCreated + ", lastUpdated="
+				+ lastUpdated + "]";
+	}
+	
 }
--- user-notes-dao/src/main/java/com/gotprint/domain/User.java
+++ user-notes-dao/src/main/java/com/gotprint/domain/User.java
@@ -1,5 +1,60 @@
 package com.gotprint.domain;
 
+import java.util.Date;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "note")
 public class User {
+	
+	@Id
+	@Column(name = "userEmail")
+	private String userEmail;
+	
+	@Column(name = "password")
+	private String password;
+	
+	@Column(name = "timeCreated")
+	private Date timeCreated;
+	
+	@Column(name = "lastUpdated")
+	private Date lastUpdatedTime;
+	
+	@OneToMany(mappedBy="user")
+	private Set<Note> note;
+	
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Date getTimeCreated() {
+		return timeCreated;
+	}
+	public void setTimeCreated(Date timeCreated) {
+		this.timeCreated = timeCreated;
+	}
+	public Date getLastUpdatedTime() {
+		return lastUpdatedTime;
+	}
+	public void setLastUpdatedTime(Date lastUpdatedTime) {
+		this.lastUpdatedTime = lastUpdatedTime;
+	}
+	
+	
 
 }
